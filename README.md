a76966f78aa84d607aa82af27e2863fd6ed61645
Victor Powell <vp@uber.com>
2016-03-02 12:25:31 -0800

    Create a playground for using react.

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# generic-playground
-A generic CommonJS playground starter
+# React Playground
+
+A place to experiment and play with React.
 
 After you fork the directory, run `npm install` and then `npm run start`. After that, you can pull up the site at `http://localhost:9966/`
--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 'use strict';
 
 var window = require('global/window');
+var document = require('global/document');
 
-window.alert('hello world!');
+var React = require('react');
+var ReactDOM = require('react-dom');
+var r = require('r-dom');
+
+
+var App = React.createClass({
+  render: function render() {
+    return r.div([
+      r.span('hello world'),
+      r.svg({width: 300, height: 300}, [
+        r.g({onClick:function() { alert('wow'); }}, [
+          r.circle({r: 10, cx: 100, cy: 100, style: {fill: 'rgba(0, 0, 0, 1)'}})
+        ])
+      ])
+    ]);
+  }
+});
+
+var reactContainer = document.createElement('div');
+document.body.style.margin = '0';
+document.body.appendChild(reactContainer);
+ReactDOM.render(r(App), reactContainer);
